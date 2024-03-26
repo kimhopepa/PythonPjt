0e03a2a983646018606ec679b54c2335560aab98
kimjh <kimjh13017@hanwha.com>
2024-03-26 13:30:49 +0900

    dd
    dd

--- TestCode/FoldUI.py
+++ TestCode/FoldUI.py
@@ -10,11 +10,6 @@ import os
 from libConfig import *
 from libFile import *
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableView, QTableWidget, QTableWidgetItem
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
-import pandas as pd
-
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
@@ -23,19 +18,6 @@ def resource_path(relative_path):
 # 3) UI 파일 연결 -> 같은 경로에 위치
 form = resource_path("Folder_UI.ui")
 form_class = uic.loadUiType(form)[0]
-
-class DataFrameTableModel(QStandardItemModel):
-    def __init__(self, data, parent=None):
-        super(DataFrameTableModel, self).__init__(parent)
-        self._data = data
-
-        self.setHorizontalHeaderLabels(data.columns)
-
-        for i in range(data.shape[0]):
-            for j in range(data.shape[1]):
-                item = QStandardItem(str(data.iloc[i, j]))
-                self.setItem(i, j, item)
-
 
 # 4) 화면을 띄우는 클래스 선언
 class WindowClass(QMainWindow, form_class):
@@ -53,52 +35,9 @@ class WindowClass(QMainWindow, form_class):
         # 2. 버튼 이벤트
         self.pushButton_Path.clicked.connect(self.set_path)
         self.pushButton_FileList.clicked.connect(self.search_file_list)
-        self.pushButton_Search.clicked.connect(self.search_version)
-
-        # 3. tablwWidget 초기화
-        # self.setGeometry(100, 100, 600, 400)
-        # self.tableWidget_version.setGeometry(50, 50, 500, 300) # 위치 설정
-        self.setWindowTitle("DataFrame in QTableView Example")
-
 
         # 3. 초기화 함수 실행
         self.loadConfig()
-
-    def search_version(self):
-        try:
-            print("search_version")
-
-            data = pd.DataFrame({
-                'Name': ['Alice', 'Bob', 'Charlie'],
-                'Age': [25, 30, 35],
-                'Gender': ['Female', 'Male', 'Male']
-            })
-
-            # DataFrame을 테이블 위젯에 추가
-            # 행, 열 수 설정
-            self.tableWidget_version.setRowCount(data.shape[0])
-            self.tableWidget_version.setColumnCount(data.shape[1])
-
-            # 헤더 설정
-            self.tableWidget_version.setHorizontalHeaderLabels(data.columns)
-            self.tableWidget_version.setVerticalHeaderLabels(data.index.astype(str))
-
-            # 데이터 넣기
-            for i in range(data.shape[0]):
-                for j in range(data.shape[1]):
-                    item = QTableWidgetItem(str(data.iloc[i, j]))
-                    self.tableWidget_version.setItem(i, j, item)
-
-            # QTableView에도 데이터 추가
-            # self.tableWidget_version = QTableView(self)
-            # self.tableWidget_version.setGeometry(50, 50, 500, 300)
-            #
-            # model = DataFrameTableModel(data)
-            # self.tableWidget_version.setModel(model)
-
-        except Exception as e:
-            print("search_version() Error", e)
-
 
     def loadConfig(self):
         try:
