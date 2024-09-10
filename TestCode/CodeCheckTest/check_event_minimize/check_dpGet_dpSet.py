e0dd828910c3c8ea972bf761b23174be05e097bb
kimjh <kimjh13017@hanwha.com>
2024-09-10 18:21:39 +0900

    dd
    ddd

--- TestCode/CodeCheckTest/check_event_minimize/check_dpGet_dpSet.py
+++ TestCode/CodeCheckTest/check_event_minimize/check_dpGet_dpSet.py
@@ -28,17 +28,20 @@ def find_consecutive_lines(text, search_string):
     for line_number, line_text in enumerate(lines, start=1):
         # if prev_line_number is not None and search_string in line and search_string in lines[prev_line_number-1]:
         if prev_line_number is not None :
-            consecutive_lines = check_functions( line_text, lines[prev_line_number-1], ["dpGet", "dpSet"])
+            if check_functions( line_text, lines[prev_line_number-1], ["dpGet", "dpSet"]) == True :
+                consecutive_lines = consecutive_lines + [[line_number, line_text]]
+
         prev_line_number = line_number
     return consecutive_lines
 
 # 이전, 현재 라인 코드 비교하여 체크할 문자열이 포함되어 있으면 배열 저장
-def check_functions(current_text:str, prev_text:str, check_list:list):
+def check_functions(current_text:str, prev_text:str, check_list:list) -> bool:
+    result_list = []
     #1. 체크할 문자열 리스트 동작
     for check_text in check_list :
         #2. 이전 라인, 현재 라인 포함하는지 조건 확인
         if check_text in prev_text and check_text in current_text :
-            print("prev_text = " + prev_text, "current_text = " + current_text )
+           return True
 
 if __name__ == '__main__':
 
